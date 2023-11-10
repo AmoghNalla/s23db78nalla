4f1145c55b22ca16bf3be5b1ee5c056fbffbb72b
AmoghNalla <s566278@nwmissouri.edu>
2023-11-10 03:24:08 -0600

    done until screenshot 3

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -4,15 +4,19 @@ exports.hotel_list = function(req, res) {
 res.send('NOT IMPLEMENTED: hotel list');
 };
 // for a specific hotel.
+// exports.hotel_detail=function(req,res){
+//res.send('NOT IMPLEMENTED: hotel detail:' +req.params.id);
+
+//for a specific hotel
 exports.hotel_detail =async  function(req, res) {
-console.log("detail"+req.params.id)
-try{
+    console.log("detail"+req.params.id)
+    try{
     result=await hotel.findById(req.params.id)
     res.send(result)
-}catch(error){
+    }catch(error){
     res.status(500)
     res.send(`{"error":document for id${req.params.id} not found`);
-}
+    }
 };
 // Handle hotel create on POST.
 exports.hotel_create_post = function(req, res) {
@@ -22,9 +26,23 @@ res.send('NOT IMPLEMENTED: hotel create POST');
 exports.hotel_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: hotel delete DELETE ' + req.params.id);
 };
+
 // Handle hotel update form on PUT.
-exports.hotel_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: hotel update PUT' + req.params.id);
+exports.hotel_update_put= async function(req,res){
+console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try{
+    let toUpdate=await hotel.findById(req.params.id)
+    if(req.body.name)
+    toUpdate.name=req.body.name;
+    if(req.body.numOfRooms) toUpdate.numOfRooms=req.body.numOfRooms;
+    if(req.body.rate) toUpdate.rate=req.body.rate;
+    let result=await toUpdate.save();
+    console.log("Success "+result)
+    res.send(result)
+    }catch(err){
+    res.status(500)
+    res.send(`{"error":${err}:Update for id ${req.params.id} failed`);
+    }
 };
 
 exports.hotel_list = async function(req, res) {
@@ -48,45 +66,24 @@ exports.hotel_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
         }
         };
-
-    exports.costume_create_post = async function(req, res) {
-console.log(req.body)
-let document = new Costume();
-// We are looking for a body, since POST does not have query parameters.
-// Even though bodies can be in many different formats, we will be picky
-// and require that it be a json object
-// {"costume_type":"goat", "cost":12, "size":"large"}
-document.costume_type = req.body.costume_type;
-document.cost = req.body.cost;
-document.size = req.body.size;
-try{
-let result = await document.save();
-res.send(result);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
-};
-
 exports.hotel_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new hotel();
-    // {"costume_type":"goat", "cost":12, "size":"large"}
-    document.name = req.body.name;
-    document.numOfRooms = req.body.numOfRooms;
-    document.rate = req.body.rate;
-    try{
-    let result = await document.save();
-    res.send(result);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
-    
-
+     console.log(req.body)
+     let document = new hotel();
+      // We are looking for a body, since POST does not have query parameters.
+      // Even though bodies can be in many different formats, we will be picky
+     // and require that it be a json object
+     document.name = req.body.name;
+     document.numOfRooms = req.body.numOfRooms;
+     document.rate = req.body.rate;
+     try{
+     let result = await document.save();
+     res.send(result);
+     }
+     catch(err){
+     res.status(500);
+     res.send(`{"error": ${err}}`);
+     }
+   };
 
 
     
